10615babcc95925fdb3c0df0b9749b11df320a27
kristal <crystal_lsj@naver.com>
2024-05-21 10:40:58 +0900

    ✨ Exchange feature (without USD)
    미국 달러 외에 currency convert 잘 되는 거 확인

--- zizeon-frontend/src/stores/counter.js
+++ zizeon-frontend/src/stores/counter.js
@@ -1,7 +1,21 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import axios from 'axios'
 
 export const useCounterStore = defineStore('counter', () => {
+  const currencies = ref([])
+  const API_URL = 'http://70.12.102.186:8000'
 
-  return {  }
+  const getCurrency = function() {
+      axios({
+          method:'get',
+          url: `${API_URL}/currencies/exchange/`
+      })
+      .then(res => {
+          console.log(res.data)
+          currencies.value = res.data
+      })
+      .catch(err => console.log(err))
+  }
+  return { currencies, getCurrency }
 }, { persist : true } )
