323dea4a5a7c5a9b846e6c56e8d1137403700f78
kristal <crystal_lsj@naver.com>
2024-05-22 11:36:41 +0900

    ✨ Recommend view (only saving)
    Now users can see RECOMMENDED PRODUCTS (only SAVING)

--- zizeon-frontend/src/views/RecommendView.vue
+++ zizeon-frontend/src/views/RecommendView.vue
@@ -1,11 +1,37 @@
 <template>
-    <div>
-        <h1>추천 상품 조회</h1>
-    </div>
+  <div>
+    <h1>추천 상품 조회</h1>
+    <h3>추천 적금 목록</h3>
+    <ul>
+      <li v-for="saving in recommendSavings" :key="saving.id">
+        <p>상품명: <RouterLink :to="{ name: 'SavingDetail', params: { id: saving.id } }">{{ saving.fin_prdt_nm }}</RouterLink></p>
+        <p>회사명: {{ saving.kor_co_nm }}</p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
+  import axios from 'axios'
+  import { onMounted, ref } from 'vue'
+  import { useUserStore } from '@/stores/user'
 
+  const store = useUserStore()
+
+  const recommendSavings = ref([])
+  
+  onMounted(() => {
+    axios({
+      method: 'get',
+      url: `${store.API_URL}/products/saving/recommend`,
+    })
+      .then((response) => {
+        recommendSavings.value = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  })
 </script>
 
 <style scoped>
