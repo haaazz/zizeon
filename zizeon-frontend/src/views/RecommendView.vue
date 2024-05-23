3898f82ee1375f4d5801af7aaddffbbc64289af5
kristal <crystal_lsj@naver.com>
2024-05-23 14:43:49 +0900

    ✨ Add deposit products recommendation feature

--- zizeon-frontend/src/views/RecommendView.vue
+++ zizeon-frontend/src/views/RecommendView.vue
@@ -1,15 +1,28 @@
 <template>
   <div>
     <h1>추천 상품 조회</h1>
+
+    <h3>추천 예금 목록</h3>
+    <ul>
+      <li v-for="deposit in recommendDeposits" :key="deposit.id">
+        <p>
+          상품명:
+          <RouterLink :to="{ name: 'DepositDetail', params: { id: deposit.id } }">
+            {{ deposit.fin_prdt_nm }}
+          </RouterLink>
+        </p>
+        <p>회사명: {{ deposit.kor_co_nm }}</p>
+      </li>
+    </ul>
+    
     <h3>추천 적금 목록</h3>
     <ul>
       <li v-for="saving in recommendSavings" :key="saving.id">
         <p>
           상품명:
-          <RouterLink
-            :to="{ name: 'SavingDetail', params: { id: saving.id } }"
-            >{{ saving.fin_prdt_nm }}</RouterLink
-          >
+          <RouterLink :to="{ name: 'SavingDetail', params: { id: saving.id } }">
+            {{ saving.fin_prdt_nm }}
+          </RouterLink>
         </p>
         <p>회사명: {{ saving.kor_co_nm }}</p>
       </li>
@@ -18,13 +31,14 @@
 </template>
 
 <script setup>
-import axios from "axios";
-import { onMounted, ref } from "vue";
-import { useUserStore } from "@/stores/user";
+import axios from "axios"
+import { onMounted, ref } from "vue"
+import { useUserStore } from "@/stores/user"
 
-const store = useUserStore();
+const store = useUserStore()
 
-const recommendSavings = ref([]);
+const recommendSavings = ref([])
+const recommendDeposits = ref([])
 
 onMounted(() => {
   axios({
@@ -32,12 +46,22 @@ onMounted(() => {
     url: `${store.API_URL}/products/saving/recommend`,
   })
     .then((response) => {
-      recommendSavings.value = response.data;
+      recommendSavings.value = response.data
     })
     .catch((error) => {
-      console.log(error);
-    });
-});
+      console.log(error)
+    })
+    axios({
+      method: "get",
+      url: `${store.API_URL}/products/deposit/recommend`,
+    })
+      .then((response) => {
+        recommendDeposits.value = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+})
 </script>
 
 <style scoped></style>
