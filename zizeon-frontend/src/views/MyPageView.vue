b1a9e4b6a425d1840fa403828d465164dd9cb1b9
kristal <crystal_lsj@naver.com>
2024-05-23 10:36:22 +0900

    🚧  Personal Recommendation UI(?) added / Graph stuff is being worked in progress

--- zizeon-frontend/src/views/MyPageView.vue
+++ zizeon-frontend/src/views/MyPageView.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>마이페이지</h1>
     <button @click="ProfileEdit">회원 정보 수정</button>
+
     <h3>가입한 적금 목록</h3>
     <ul>
       <li v-for="saving in savings" :key="saving.id">
@@ -22,6 +23,25 @@
       </li>
     </ul>
 
+    <hr>
+    <h3>금리 비교 그래프</h3>
+    <hr>
+
+    <h3>추천 예금 목록</h3>
+    <ul>
+      <li v-for="deposit in recommendDeposits" :key="deposit.id">
+        <p>상품명: <RouterLink :to="{ name: 'DepositDetail', params: { id: deposit.id } }">{{ deposit.fin_prdt_nm }}</RouterLink></p>
+        <p>회사명: {{ deposit.kor_co_nm }}</p>
+      </li>
+    </ul>
+
+    <h3>추천 적금 목록</h3>
+    <ul>
+      <li v-for="saving in recommendSavings" :key="saving.id">
+        <p>상품명: <RouterLink :to="{ name: 'SavingDetail', params: { id: saving.id } }">{{ saving.fin_prdt_nm }}</RouterLink></p>
+        <p>회사명: {{ saving.kor_co_nm }}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -73,5 +93,8 @@
 </script>
 
 <style scoped>
-
+  h3 {
+    font-size: 20pt;
+    margin: 10px 0;
+  }
 </style>
