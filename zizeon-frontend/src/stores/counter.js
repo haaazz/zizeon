a72e2062d99af1f5d048e5d8c62265cbd8930dff
haaazz <ez703@naver.com>
2024-05-24 09:35:23 +0900

    ✨ hover

--- zizeon-frontend/src/stores/counter.js
+++ zizeon-frontend/src/stores/counter.js
@@ -6,11 +6,7 @@ export const useCounterStore = defineStore(
   "counter",
   () => {
     const currencies = ref([]);
-<<<<<<< HEAD
-    const API_URL = "http://70.12.102.186:8000";
-=======
     const API_URL = 'http://70.12.102.186:8000';
->>>>>>> b2d2aa7b00a2582c49e86d3982ec49c4b264a279
 
     const getCurrency = function () {
       axios({
